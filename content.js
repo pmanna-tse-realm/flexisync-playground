c51ee1da61aa0c726c72900cfc855331b9ea1112
Paolo <paolo.manna@mongodb.com>
2022-07-13 12:09:47 +0100

    Added simple object visualisation

--- content.js
+++ content.js
@@ -3,6 +3,7 @@ const main = require("./main");
 const index = require("./index");
 const config = require("./config");
 const output = require("./output");
+const inquirer = require("inquirer");
 
 function queryClasses(realm) {
   const realm_schema = realm.schema;
@@ -22,20 +23,98 @@ function trackClass(realm, className) {
   output.watchResult(`${className}`, `${objects.length} objects`);
 }
 
+async function showTableContent(realm, className) {
+  // Get all objects for that class, and print them, with an upper limit of 10
+  const objects = realm.objects(className);
+  const count = objects.length < 10 ? objects.length : 10;
+  let index = 0;
+  let fields = [];
+  let samples = [];
+
+  for (let object of objects) {
+    if (index == 0) {
+      // At the beginning, fetch the internal schema of the object
+      let schema = object.objectSchema();
+
+      fields = Object.keys(schema.properties);
+    }
+
+    let objDesc = {};
+
+    // Build a readable sample of the object
+    fields.forEach(field => {
+      const value = object[field];
+      const type = typeof value;
+
+      // Showing non-basic types is confusing, so we filter for common readable types + _id
+      switch (type) {
+        case 'object':
+          if (field == '_id') { objDesc[field] = value.toHexString(); }
+          break;
+        case 'string':
+          if (value.length <= 30) {
+            objDesc[field] = value;
+          } else {
+            objDesc[field] = value.substring(0, 29) + '…';
+          }
+          break;
+        case 'number':
+          objDesc[field] = value;
+          break;
+        default:
+          break;
+      }
+    });
+
+    samples.push(objDesc);
+
+    index += 1;
+
+    if (index >= count) {
+      break;
+    }
+  }
+
+  output.table(samples);
+
+  await main.waitForKey();
+}
+
 async function showContent() {
   let realm = await index.getRealm();
 
   if (realm == undefined) { return; }
 
   let synchedClasses = queryClasses(realm);
+  let classNames = [];
 
   for (const objSchema of synchedClasses) {
     if (!objSchema['embedded']) {
-      trackClass(realm, objSchema['name']);
+      classNames.push(objSchema['name']);
     }
   }
 
-  await main.waitForKey();
+  classNames.forEach(name => {
+    trackClass(realm, name);
+  });
+
+
+  let choice = await inquirer.prompt([
+    {
+      type: "rawlist",
+      name: "select",
+      message: "Choose a table to get a sample from:",
+      choices: [...classNames, new inquirer.Separator(), "Back"]
+    },
+  ]);
+
+  switch (choice.select) {
+    case 'Back':
+      return;
+    default:
+      await showTableContent(realm, choice.select);
+      break;
+  }
 }
 
 exports.showContent = showContent;
